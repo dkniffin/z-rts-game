b3ab8e469ef5afe91fde51c5d48ec5b321adb42c
Derek Kniffin <kniffin@colorado.edu>
2013-05-28 12:17:05 -0600

    Added the "/nick" command to the chat box. Added the associated code to server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 // Include some libraries
 var io = require('socket.io').listen(1113), // Set up socket.io to listen on port 1113
-    pg = require('pg'),
-    mdb = require('./map-db.js');
-io.set('log level', 1);
+    mdb = require('./map-db.js'),
+    gdb = require('./game-db.js'),
+    chat = require('./chat.js'),
+    usrlib = require('./users.js');
+io.set('log level', 1); // Don't be as detailed in logging
 
 // Set up some global variables
 var users = [];
@@ -10,9 +12,12 @@ var users = [];
 /*========================Set up connection handler========================*/
 // New connections
 io.sockets.on('connection', function (socket) {
-  console.log('connection made');
+  console.log('New Connection');
+  var displayName = ''
+  
   socket.on('reconnect', function(data){
-    if (typeof(data.sKey) != "undefined"){
+    console.log('A connection is trying to reconnect...');
+    if (typeof data.sKey  != "undefined"){
       // See if session was already authenticated.
       // If it was...
       //socket.set('authenticated', true);
@@ -20,16 +25,17 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('auth', function (data){
+    console.log('User is authenticating...');
     // A user would like to authenticate
 
     // Check to see if the sent the right data; else return error.
-    if (!validateAuthDate){
+    if (!users.validateAuthData){
       socket.emit('auth-error', { e: 'either username or password was not defined'});
       return;
     }
     
     // data is correct. Authenticate.
-    var authRet = authUser(data.uname, data.pwhash);
+    var authRet = usrlib.authUser(data.uname, data.pwhash);
     
     // If it returned an error, return to client
     if(authRet.error == true){
@@ -37,12 +43,21 @@ io.sockets.on('connection', function (socket) {
       return;
     }
     
+    // Set authenticated to true
     socket.set('authenticated', true);
     // Generate a session key
 
     // User is authenticated. Add to array of users, and send back data
-    var user = addUser({ username: data.uname });
+    var user = usrlib.addUser({ username: data.uname });
     socket.set('user', user);
+  });
+  
+  socket.on('setDisplayName', function(data){
+    // A user wants to set their username for chat
+    console.log('User wants to change display name to '+data.displayName);
+    if (typeof data.displayName != "undefined" && data.displayName != ''){
+         displayName = data.displayName;
+      }
   });
 
   var b;
@@ -50,84 +65,30 @@ io.sockets.on('connection', function (socket) {
     console.log('client requested all buildings');
     mdb.getAllBuildings(function(b){
       socket.emit('allBuildings', {
-	bldgs: b
+        bldgs: b
       });
     }); // get the geoJSON for the buildings
   });
 
   socket.on('message', function(data){
     console.log('message recieved: '+data.message);
-    //if (socket.get('authenticated') == true){
-      if (typeof(data.message) != "undefined" && data.message != ''){
-	io.sockets.emit('update-chat', {
-	  //uname: socket.get('user').username,
-	  uname: 'bob',
-	  msg: data.message
-	});
-      }
-    //}
+    usrlib.checkAuth(socket);
+     if (typeof data.message != "undefined" && data.message != ''){
+      io.sockets.emit('update-chat', {
+         uname: (typeof displayName != "undefined") ? displayName : "Unknown user",
+         msg: data.message
+      });
+     }
   });
   
   socket.on('action', function (data) {
-    if (socket.get('authenticated') == true){
+    //if (socket.get('authenticated') == true){
       // The client has sent some action it'd like to perform
       // Process the action, and send some updates to all clients
       actionHandler(data);
-    }
+    //}
   });
 });
-
-/*========================User management========================*/
-
-var validateAuthData = function(data){
-  if (typeof(data.uname) != "undefined" && typeof(data.pwhash) != "undefined"){
-    return true;
-  }
-  return false;
-}
-
-var authUser = function(username, pwHash){
-  var retVal = {};
-
-  // Check the DB, etc
-
-  retVal.errString = ''; // If there's an error, put the string here.
-  retVal.error = false; // true = error, false = no error. For now, no errors
-
-  return retVal;
-}
-
-var addUser = function(options){
-  var user = {};
-  user.username = options.username; // Add the username to the object
-
-  // Pull rest of user info from DB
-  user.id = '1234'; // This'll come from DB later
-
-  users.push(user); // Add the user to the users array
-  
-  sendNewUser(user); // Send new userlist out to all
-  return user;
-}
-
-var sendNewUser = function(user) {
-  io.sockets.emit('new-user', { uname: user.username, id: user.id });
-}
-
-var removeUser = function(remUid){
-  // Remove user
-  for(var i = 0; i < users.length; ++i) {
-    if(users[i].id == remUid) {
-	users.splice(i,1);
-	sendRemUser(remUid);
-        break;
-    }
-  }
-}
-
-var sendRemUser = function(remUid){
-  io.sockets.emit('rem-user', { id: remUid });
-}
 
 /*========================Action Handlers========================*/
 
---
+++ server/users.js
@@ -0,0 +1,63 @@
+/*========================User management========================*/
+
+exports.validateAuthData = function(data){
+  if (typeof data.uname != "undefined" && typeof data.pwhash != "undefined"){
+    return true;
+  }
+  return false;
+}
+
+exports.authUser = function(username, pwHash){
+  var retVal = {};
+
+  // Check the DB, etc
+
+  retVal.errString = ''; // If there's an error, put the string here.
+  retVal.error = false; // true = error, false = no error. For now, no errors
+
+  return retVal;
+}
+
+exports.addUser = function(options){
+  var user = {};
+  user.username = options.username; // Add the username to the object
+
+  // Pull rest of user info from DB
+  user.id = '1234'; // This'll come from DB later
+
+  users.push(user); // Add the user to the users array
+  
+  sendNewUser(user); // Send new userlist out to all
+  return user;
+}
+
+exports.sendNewUser = function(io,user) {
+  io.sockets.emit('new-user', { uname: user.username, id: user.id });
+}
+
+exports.removeUser = function(remUid){
+  // Remove user
+  for(var i = 0; i < users.length; ++i) {
+    if(users[i].id == remUid) {
+	users.splice(i,1);
+	sendRemUser(remUid);
+        break;
+    }
+  }
+}
+
+exports.sendRemUser = function(io,remUid){
+  io.sockets.emit('rem-user', { id: remUid });
+}
+
+exports.setDisplayName = function(socket,data){
+  if (typeof data.displayName != "undefined" && data.displayName != ''){
+    var user = socket.get('user');
+    user.displayName = data.displayName;
+    socket.set('user',user);
+  }
+}
+
+exports.checkAuth = function(socket){
+   if (!socket.get('authenticated'){ socket.emit('error', {e: 'Not Authenticated'});
+}
